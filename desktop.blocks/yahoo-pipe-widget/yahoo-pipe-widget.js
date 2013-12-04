52dd93ea9951d0149ef49fe51e6e555b4b9e46ad
Varvara Stepanova <toivonen@toivonen.ru>
2013-12-04 21:58:07 +0100

    Comments on JavaScript code

--- desktop.blocks/yahoo-pipe-widget/yahoo-pipe-widget.js
+++ desktop.blocks/yahoo-pipe-widget/yahoo-pipe-widget.js
@@ -3,11 +3,15 @@
 (function(){
 
 var urlParser = function(url) {
+    // Fake "a" node for parsing URLs
     var parser = document.createElement('a');
     parser.href = url;
     return parser;
 }
 
+/**
+ * Merges host and relative path
+ */
 var imageUrl = function(host, image) {
     if (!image) return;
 
@@ -51,9 +55,21 @@ BEM.DOM.decl('yahoo-pipe-widget', {
                         _render: 'json'
                     },
                     function(data) {
+                        /**
+                         * Change widget status when data is loaded
+                         */
                         this.setMod('status', 'loaded');
+
+                        /**
+                         * From data for the list
+                         */
                         var list = [],
                             items = data.value.items;
+
+                        /**
+                         * Limitation in request is missing,
+                         * loops is a hack here
+                         */
                         for (var i=0;i<10;i++){
                             var item = items[i];
                             list.push({
@@ -63,9 +79,13 @@ BEM.DOM.decl('yahoo-pipe-widget', {
                                 link: item.link
                             });
                         }
+
                         this.createList(list);
                     },
                     function() {
+                        /**
+                         * Change widget status in case of error
+                         */
                         this.setMod('status', 'error');
                     }
                 )
@@ -77,7 +97,13 @@ BEM.DOM.decl('yahoo-pipe-widget', {
             }
         }
     },
+
+    /**
+     * Creates View-oriented JSON from
+     * data
+     */
     createList: function(list){
+
         var listBlocks = $.map(list, function(item){
             return {
                 block: 'yahoo-pipe-widget',
@@ -104,12 +130,29 @@ BEM.DOM.decl('yahoo-pipe-widget', {
                 ]
             }
         });
+
+        /**
+         * Templates are applied to View-oriented JSON and then
+         * the result is inserted into the widget
+         */
         BEM.DOM.append(this.domElem, BEMHTML.apply(listBlocks));
     },
+
+    /**
+     * Sets current item
+     */
     setCurrent: function(item) {
+
+        /**
+         * Previously opened item shoud be closed
+         */
         if (this._currentItem) {
             this.delMod(this._currentItem, 'opened', 'true');
         }
+
+        /**
+         * Change item state
+         */
         this.setMod(item, 'opened', 'true');
         this._currentItem = item;
     }
@@ -118,6 +161,9 @@ BEM.DOM.decl('yahoo-pipe-widget', {
 
     live: function() {
 
+        /**
+         * Binding to "live" click event, using delegation
+         */
         this.liveBindTo('item', 'click', function(e){
             this.setCurrent(e.data.domElem);
         });
